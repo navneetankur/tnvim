7dfa0747a07aefd39610956470da9fb178fe2fd9
Navneet Aman <contact@navneetaman.com>
2025-12-05 16:21:44 +0530

    update

--- nvimapisys/src/error.rs
+++ nvimapisys/src/error.rs
@@ -5,9 +5,9 @@ use rmpv::Value;
 pub type Result<T> = core::result::Result<T, Error>;
 #[derive(Debug)]
 pub struct Error {
-    msg: Cow<'static, str>,
-    inner: Option<Box<dyn core::error::Error>>,
-    inner_value: Option<Box<Value>>,
+    pub msg: Cow<'static, str>,
+    pub inner: Option<Box<dyn core::error::Error>>,
+    pub inner_value: Option<Box<Value>>,
 }
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -43,13 +43,23 @@ pub fn with_msg_inner<T>(msg: impl Into<Cow<'static, str>>, inner: impl core::er
         Error::new(msg, Some(inner), None)
     )
 }
+pub fn with_value<T>(value: Value) -> Result<T> {
+    Err(
+        Error::from_value(value)
+    )
+}
+pub fn with_msg_value<T>(msg: impl Into<Cow<'static, str>>, value: Value) -> Result<T> {
+    Err(
+        Error::from_msg_value(msg, value)
+    )
+}
 impl Error {
-    pub fn new(msg: impl Into<Cow<'static, str>>, inner: Option<impl core::error::Error + 'static>, inner_value: Option<Box<Value>>) -> Self {
+    pub fn new(msg: impl Into<Cow<'static, str>>, inner: Option<impl core::error::Error + 'static>, inner_value: Option<Value>) -> Self {
         let inner: Option<Box<dyn core::error::Error + 'static>> = 
             if let Some(inner) = inner {
                 Some(Box::new(inner))
             } else {None};
-        Self { msg: msg.into(), inner: inner, inner_value }
+        Self { msg: msg.into(), inner: inner, inner_value: inner_value.map(Box::new) }
     }
 
     pub fn from_inner(inner: impl core::error::Error + 'static) -> Self {
@@ -60,6 +70,9 @@ impl Error {
     }
     pub fn from_value(value: Value) -> Self {
         Error { msg: "".into(), inner: None, inner_value: Some(Box::new(value)) }
+    }
+    pub fn from_msg_value(msg: impl Into<Cow<'static, str>>, value: Value) -> Self {
+        Error { msg: msg.into(), inner: None, inner_value: Some(Box::new(value)) }
     }
 }
 
